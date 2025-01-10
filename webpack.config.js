76d2e6dbb9a55b5bb180aaac3044d3d1a378c9fc
Stefano <30480337+snebo@users.noreply.github.com>
2025-01-10 16:57:45 +0100

    add bootstrap and scss file

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,73 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const DotenvWebpackPlugin = require("dotenv-webpack");
+'use strict';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DotenvWebpackPlugin = require('dotenv-webpack');
+const autoprefixer = require('autoprefixer');
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-        clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["src/**/*"], // this updates when anythin gchanges
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            // link the file here
-            // template: "./src/templates/index.html",
-        }),
-        new DotenvWebpackPlugin({
-            systemvars: true,
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ]
-    }
-}
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.js',
+		clean: true,
+	},
+	devtool: 'eval-source-map',
+	devServer: {
+		watchFiles: ['src/**/*'], // this updates when anythin gchanges
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			// link the file here
+			template: './src/index.html',
+		}),
+		new DotenvWebpackPlugin({
+			systemvars: true,
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.html$/i,
+				loader: 'html-loader',
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(scss)$/,
+				use: [
+					{
+						// Adds CSS to the DOM by injecting a `<style>` tag
+						loader: 'style-loader',
+					},
+					{
+						// Interprets `@import` and `url()` like `import/require()` and will resolve them
+						loader: 'css-loader',
+					},
+					{
+						// Loader for webpack to process CSS with PostCSS
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [autoprefixer],
+							},
+						},
+					},
+					{
+						// Loads a SASS/SCSS file and compiles it to CSS
+						loader: 'sass-loader',
+						options: {
+							// Prefer `dart-sass`
+							implementation: require('sass'),
+						},
+					},
+				],
+			},
+		],
+	},
+};
