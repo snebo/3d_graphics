6a951a1703f148d06f154cd91b082ba161e002b9
Stefano <30480337+snebo@users.noreply.github.com>
2025-01-10 22:37:36 +0100

    add navigation section

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import './scss/styles.scss';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
+// document.addEventListener("DOMContentLoaded", ()=>{
+//     const nav_btn = document.getElementById('fullScreenNavModal');
+//     nav_btn.addEventListener('click', ())
+// })
